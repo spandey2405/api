9285a9e280adba7733a5567bd34717b311933ec1
spandey2405 <spandey2405@gmail.com>
2015-09-16 02:23:53 +0530

    Added Insert Fuction

--- v1.0/insert.php
+++ v1.0/insert.php
@@ -1,8 +1,62 @@
 <?php
+require 'connect/connect.php';
+require 'keys/keys.php';
+$data['success'] = 0;
+if(isset($_GET["p"]))
+{
+    $p = htmlspecialchars($_GET["p"]);
+}
+else
+{
+    $p = 0;
+}
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//execute body 
 
+if(isset($_GET["key"]) && isset($_GET["title"]) && isset($_GET["file"]) && isset($_GET["by"]))
+{
+    $type = 'joke';
+    if (isset($_GET["type"]))
+    {
+        $type = htmlspecialchars($_GET["type"]);
+    }
+    
+    $key = htmlspecialchars($_GET["key"]);
+    if ($key == $key1)
+    {
+    $title = htmlspecialchars($_GET["title"]);
+    $file = htmlspecialchars($_GET["file"]);
+    $by = htmlspecialchars($_GET["by"]);
+    $t = time();
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    $sql = "INSERT INTO `funnymiku`(`title`, `type`, `filename`, `time`, `postedby`) VALUES ('$title','$type','$file',$t,'$by')";
+    if (mysqli_query($conn, $sql)) {
+        $data['success'] = 1;
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+    mysqli_close($conn); 
+    }
+    else
+    {
+        $data['error']="Invalid Authentication";
+    }
+
+
+}
+else
+{
+    $data['error']="Arguments Missing";
+}
+
+//print responce
+
+if($p==1)
+{
+   echo json_encode($data);
+}
+else
+{
+   echo "<pre>".json_encode(($data),JSON_PRETTY_PRINT)."</pre>";
+}
+?>
